342dd23c7a06b0fbc731c4cb6b98daa6e8f38e56
Mitch LeBlanc <supermitch@gmail.com>
2015-01-17 22:56:08 -1000

    Rename pairs as sets
    Need to make sure I can generalize to multiple odd nodes, not just 2 pairs of 2

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import random
 import eularian
 import my_math
 
-def find_pair_cost(pair, graph):
+def find_set_cost(path_set, graph):
     """ Find the total cost of a set of path options. """
-    return sum(find_cost(path, graph) for path in pair)
+    return sum(find_cost(path, graph) for path in path_set)
 
 def find_cost(path, graph):
     """ Return minimum cost from start to end nodes. """
     start, end = path
     print('path: {}{}'.format(start, end))
-    # Implement Dijkstra's?
+    # Implement Dijkstra's
     return 1
 
 def make_eularian(graph):
@@ -20,12 +20,19 @@ def make_eularian(graph):
     print('Odd nodes: {}'.format(odd_nodes))
     combos = list(itertools.combinations(sorted(odd_nodes), 2))
     print('Combos: {}'.format(list(combos)))
-    pairs = [pair for pair in itertools.combinations(combos, 2) if \
+    # TODO: This doesn't really work for sets of more than 2! Fix it.
+    # It sucks anyway.
+    path_sets = [pair for pair in itertools.combinations(combos, 2) if \
              pair[0][0] not in pair[1] and pair[0][1] not in pair[1]]
-    print('Possible pairs: {}'.format(list(pairs)))
+    print('Possible pairs: {}'.format(list(path_sets)))
     # Now find the shortest distances from node to node for each pair
-    costs = [find_pair_cost(pair, graph) for pair in pairs]
+    costs = [find_set_cost(path_set, graph) for path_set in path_sets]
     print('Costs: {}'.format(list(costs)))
+
+    min_cost = min(costs)
+    print('Minimum cost: {}'.format(min_cost))
+    optimum_set = path_sets[costs.index(min(costs))]
+    print('Optimum path set: {}'.format(optimum_set))
 
     new_graph = graph
     return new_graph
