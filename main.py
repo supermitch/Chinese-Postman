d87e0f772dd3401c20e24172e28b78f51373394b
Mitch LeBlanc <supermitch@gmail.com>
2015-01-19 21:02:01 -1000

    Found unique path sets.
    Figured out how to find all combos of path sets without duplicates.

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import random
 import eularian
 import my_math
 
+
+def flatten_tuples(iterable):
+    """
+    Flatten an iterable containing several tuples.
+
+    It works because you can add a tuple to another to extend them,
+    and sum can use the empty tuple () as the start value.
+
+    """
+    return sum(iterable, ())
+
+def all_unique(iterable):
+    """ Returns True if all items in an iterable are unique. """
+    seen = set()
+    return not any (i in seen or seen.add(i) for i in iterable)
+
 def find_set_cost(path_set, graph):
     """ Find the total cost of a set of path options. """
     return sum(find_cost(path, graph) for path in path_set)
@@ -17,34 +33,31 @@ def find_cost(path, graph):
 
 def make_eularian(graph):
     odd_nodes = eularian.find_odd_nodes(graph)
-    print('Odd nodes: {}'.format(odd_nodes))
+    print('Odd nodes: {}'.format(sorted(odd_nodes)))
     combos = list(itertools.combinations(sorted(odd_nodes), 2))
     print('Combos: {}'.format(list(combos)))
     # TODO: This doesn't really work for sets of more than 2! Fix it.
     # It sucks anyway.
-    path_set_a = []
-    for pair in itertools.combinations(combos, 2):
-        for node in pair[0]:
-            print(pair[0], node, pair[1])
-            if node in pair[1]:
-                continue
-            path_set_a.append(pair)
-    path_set_b = [pair for pair in itertools.combinations(combos, 2) if \
-                  not any(node in pair[1] for node in pair[0])]
-    path_sets = [pair for pair in itertools.combinations(combos, 2) if \
-             pair[0][0] not in pair[1] and pair[0][1] not in pair[1]]
-    print('Possible pairs: {}'.format(list(path_sets)))
-    print('Possible pairs A: {}'.format(list(path_set_a)))
-    print('Possible pairs B: {}'.format(list(path_set_b)))
+    no_of_sets = len(odd_nodes) / 2
+
+    path_sets = [path_set for path_set in \
+                 itertools.combinations(combos, no_of_sets) \
+                 if all_unique(flatten_tuples(path_set))]
+    print('Possible pairs B: {}'.format(path_sets))
+
+    #              not any(node in [x for x in pair[1:] for node in pair[0])]
+    #path_sets = [pair for pair in itertools.combinations(combos, 2) if \
+    #         pair[0][0] not in pair[1] and pair[0][1] not in pair[1]]
+    #print('Possible pairs A: {}'.format(list(path_set_a)))
     # Now find the shortest distances from node to node for each pair
-    costs = [find_set_cost(path_set, graph) for path_set in path_sets]
-    print('Costs: {}'.format(list(costs)))
-
-    min_cost = min(costs)
-    print('Minimum cost: {}'.format(min_cost))
-    optimum_set = path_sets[costs.index(min(costs))]
-    print('Optimum path set: {}'.format(optimum_set))
-
+#    costs = [find_set_cost(path_set, graph) for path_set in path_sets]
+#    print('Costs: {}'.format(list(costs)))
+#
+#    min_cost = min(costs)
+#    print('Minimum cost: {}'.format(min_cost))
+#    optimum_set = path_sets[costs.index(min(costs))]
+#    print('Optimum path set: {}'.format(optimum_set))
+#
     new_graph = graph
     return new_graph
 
@@ -64,6 +77,20 @@ def main():
         'BD': 3,
         'CD': 3,
         'DE': 9,
+    }
+    graph = {
+        'AB': 8,
+        'AE': 4,
+        'AH': 3,
+        'BC': 9,
+        'BG': 6,
+        'CD': 5,
+        'CF': 3,
+        'DE': 5,
+        'DF': 1,
+        'EF': 2,
+        'EG': 3,
+        'GH': 1,
     }
 
     if not eularian.is_eularian(graph):
