a07a3132c45e70743ea820156c102344e70269ca
Mitch LeBlanc <supermitch@gmail.com>
2015-01-27 23:37:03 -0800

    Debugging build_path_sets

--- main.py
+++ main.py
@@ -11,11 +11,24 @@ import my_iter
 
 def build_path_sets(graph):
     """ Builds all possible sets of odd node pairs. """
+    print('finding odd nodes')
     odd_nodes = gr.find_odd_nodes(graph)
+    print('finding combos')
     combos = list(itertools.combinations(sorted(odd_nodes), 2))
     no_of_sets = len(odd_nodes) / 2
 
+    print('combos: {}'.format(combos))
+    print('len combos: {}'.format(len(combos)))
+    print('no of sets: {}'.format(no_of_sets))
     # Every pair in a set of paths must have unique nodes
+    print('finding path sets')
+    i = 0
+    import sys
+    sys.exit()
+    for path_set in itertools.combinations(combos, no_of_sets):
+        i += 1
+        print('{}: {}'.format(i, path_set))
+
     path_sets = [path_set for path_set in \
                  itertools.combinations(combos, no_of_sets) \
                  if my_iter.all_unique(my_iter.flatten_tuples(path_set))]
@@ -23,20 +36,25 @@ def build_path_sets(graph):
 
 def find_set_cost(path_set, graph):
     """ Find the cost and route for each path in a set of path options. """
+    print('finding set cost')
     return {path: dijkstra.find_cost(path, graph) for path in path_set}
 
 def find_set_solutions(path_sets, graph):
     """ Return path and cost for all paths in the path sets. """
+    print('finding set solutions')
     set_solutions = [find_set_cost(path_set, graph) for path_set in path_sets]
 
+    print('calculating set costs')
     set_costs = [sum(v[0] for v in paths.values()) for paths in set_solutions]
 
+    print('calculating set routes')
     set_routes = [list(v[1] for v in paths.values()) for paths in set_solutions]
 
     return set_routes, set_costs
 
 def find_minimum_path_set(path_sets, set_routes, set_costs):
     """ Find the cheapest of all the path set solutions. """
+    print('finding minimum set')
     min_cost = min(set_costs)
     min_set = path_sets[set_costs.index(min_cost)]
     min_route = set_routes[set_costs.index(min_cost)]
@@ -44,6 +62,7 @@ def find_minimum_path_set(path_sets, set_routes, set_costs):
 
 def add_new_edges(graph, min_route):
     """ Return new graph w/ new edges extracted from minimum route. """
+    print('adding new edges')
     new_edges = []
     for node in min_route:
         for i in range(len(node) - 1):
@@ -57,14 +76,19 @@ def add_new_edges(graph, min_route):
 def make_eularian(graph):
     """ Add necessary paths to the graph such that it becomes Eularian. """
 
+    print('\tmaking paths...')
     path_sets = build_path_sets(graph)  # Get all possible added path sets
 
+    print('\tcalculations solutions...')
     set_routes, set_costs = find_set_solutions(path_sets, graph)
 
+    print('\tfinding minimum cost...')
     min_route = find_minimum_path_set(path_sets, set_routes, set_costs)
 
+    print('\tmodifying graph...')
     new_graph = add_new_edges(graph, min_route)  # Add our new edges
 
+    print('\tdone')
     return new_graph
 
 def main():
