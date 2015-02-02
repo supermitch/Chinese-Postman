501a52d51e8f0c4abf539b455fbf70f3d76d6534
Mitch LeBlanc <supermitch@gmail.com>
2015-02-02 11:00:38 -0800

    Add graph.py tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -22,3 +22,44 @@ class TestEularian(unittest.TestCase):
         expected = ['AC', 'BC']
         self.assertEqual(expected, result)
 
+    def test_all_nodes(self):
+        graph = [('AB', 2), ('AC', 3), ('BC', 4), ('AE', 5)]
+        result = gr.all_nodes(graph)
+        expected = set(['A', 'B', 'C', 'E'])
+        self.assertEqual(expected, result)
+
+    def test_odd_nodes(self):
+        graph = [('AB', 2), ('AC', 3), ('BC', 4), ('AE', 5)]
+        result = gr.odd_nodes(graph)
+        expected = ['A', 'E']
+        self.assertEqual(expected, result)
+
+    def test_end_node_correct(self):
+        result = gr.end_node('A', 'AE')
+        self.assertEqual('E', result)
+
+    def test_end_node_incorrect(self):
+        result = gr.end_node('A', 'AE')
+        self.assertNotEqual('A', result)
+
+    def test_orders(self):
+        graph = [('AB', 2), ('AC', 3), ('BC', 4), ('AE', 5)]
+        result = gr.orders(graph)
+        expected = {'A': 3, 'B': 2, 'C': 2, 'E': 1}
+        self.assertEqual(expected, result)
+
+    def test_edge_cost_correct(self):
+        graph = [('AB', 2), ('AC', 3), ('BC', 4), ('AE', 5)]
+        result = gr.edge_cost('AC', graph)
+        self.assertEqual(3, result)
+
+    def test_edge_cost_reversed(self):
+        graph = [('AB', 2), ('AC', 3), ('BC', 4), ('AE', 5)]
+        result = gr.edge_cost('CA', graph)
+        self.assertEqual(3, result)
+
+    def test_edge_cost_not_found(self):
+        graph = [('AB', 2), ('AC', 3), ('BC', 4), ('AE', 5)]
+        result = gr.edge_cost('AD', graph)
+        self.assertIsNone(result)
+
