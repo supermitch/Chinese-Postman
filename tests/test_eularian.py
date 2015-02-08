0f735a26870be96b9af7f856f129c4ac849f65e2
Mitch LeBlanc <supermitch@gmail.com>
2015-02-07 21:36:12 -0800

    Add initial eularian.py tests

--- tests/test_eularian.py
+++ tests/test_eularian.py
@@ -4,6 +4,16 @@ import eularian
 
 class TestEularian(unittest.TestCase):
 
-    def test_test(self):
-        self.assertTrue(True)
+    def test_add_new_edges_simple_true(self):
+        graph = [('AB', 4), ('AC', 4), ('BC', 1), ('BD', 4), ('CD', 4)]
+        min_route = [['B', 'C']]
+        expected = [('AB', 4), ('AC', 4), ('BC', 1), ('BD', 4), ('CD', 4), ('BC', 1)]
+        self.assertEqual(eularian.add_new_edges(graph, min_route), expected)
+
+    def test_make_eularian_simple_true(self):
+        """ Test an obvious eularian circuit modification. """
+        graph = [('AB', 4), ('AC', 4), ('BC', 1), ('BD', 4), ('CD', 4)]
+        new, cost = eularian.make_eularian(graph)
+        expected = [('AB', 4), ('AC', 4), ('BC', 1), ('BD', 4), ('CD', 4), ('BC', 1)]
+        self.assertEqual((new, cost), (expected, 1))
 
