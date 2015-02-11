22eebf4e09ccc20ee09b107264ad8ac0adca31e7
Mitch LeBlanc <supermitch@gmail.com>
2015-02-11 14:44:02 -0800

    Simplify tests using setup

--- tests/test_network.py
+++ tests/test_network.py
@@ -5,40 +5,39 @@ import network
 class TestGraph(unittest.TestCase):
 
     def setUp(self):
-        self.graph = network.Graph([(1,2,4), (1,4,4), (2,4,1),
-                                   (2,3,4), (3,4,4)])
+        """ Set up a default graph. """
+        self.edges = [(1,2,4), (1,4,4), (2,4,1), (2,3,4), (3,4,4)]
+        self.graph = network.Graph(self.edges)
 
     def test_node_keys(self):
         self.assertEqual([1, 2, 3, 4], self.graph.node_keys)
 
     def test_node_options_correct(self):
-        graph = network.Graph([(1,2,4), (1,4,4), (2,4,1), (2,3,4), (3,4,4)])
-        self.assertEqual([(2, 4), (4, 4)], graph.node_options(1))
+        self.assertEqual([(2, 4), (4, 4)], self.graph.node_options(1))
 
     def test_all_edges_correct(self):
-        edges = [(1,2,4), (1,4,4), (2,4,1), (2,3,4), (3,4,4)]
-        graph = network.Graph(edges)
-        self.assertEqual(edges, graph.all_edges)
+        self.assertEqual(self.edges, self.graph.all_edges)
 
     def test_edge_cost_correct(self):
-        graph = network.Graph([(1,2,4), (1,4,4), (2,4,1), (2,3,4), (3,4,4)])
-        self.assertEqual(4, graph.edge_cost(1, 4))
+        self.assertEqual(4, self.graph.edge_cost(1, 4))
 
     def test_edge_cost_correct(self):
-        graph = network.Graph([(1,2,4), (1,4,4), (2,4,1), (2,3,4), (3,4,4)])
-        self.assertNotEqual(1, graph.edge_cost(1, 2))
+        self.assertNotEqual(1, self.graph.edge_cost(1, 2))
+
+    def test_edge_cost_reversed(self):
+        self.assertEqual(4, self.graph.edge_cost(4, 1))
 
     def test_total_cost_correct(self):
         self.assertEqual(17, self.graph.total_cost)
 
     def test_remove_edge_correct(self):
-        graph = network.Graph([(1,2,4), (1,4,4), (2,4,1), (2,3,4), (3,4,4)])
+        graph = network.Graph(self.edges)
         graph.remove_edge((1,4,4))
         expected = [(1,2,4), (2,4,1), (2,3,4), (3,4,4)]
         self.assertEqual(expected, graph.all_edges)
 
     def test_remove_edges_correct(self):
-        graph = network.Graph([(1,2,4), (1,4,4), (2,4,1), (2,3,4), (3,4,4)])
+        graph = network.Graph(self.edges)
         graph.remove_edges([(1,4,4), (2,3,4)])
         expected = [(1,2,4), (2,4,1), (3,4,4)]
         self.assertEqual(expected, graph.all_edges)
