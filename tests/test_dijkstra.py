a55f99df5a9da13e8884debd548ffea91d02d6f8
Mitch LeBlanc <supermitch@gmail.com>
2015-02-12 12:23:22 -0800

    Add simple Dijkstra shortest route test

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -1,13 +1,18 @@
 import unittest
 
-import dijkstra
+import dijkstra as di
+import network
 
 class TestDijkstra(unittest.TestCase):
 
     def test_summarize_path(self):
         path = {1: None, 2:1, 3:None, 4:2}
-        self.assertTrue([4, 2, 1], dijkstra.summarize_path(4, path))
+        self.assertEqual([1, 2, 4], di.summarize_path(4, path))
 
-    def test_find_cost(self):
-        self.assertTrue(True)
+    def test_find_cost_easy(self):
+        graph = network.Graph([(1,2,1), (2,3,1), (3,4,5), (4,1,5)])
+        path = (1, 3)
+        cost, route = di.find_cost(path, graph)
+        expected = (2, [1, 2, 3])
+        self.assertEqual(expected, (cost, route))
 
