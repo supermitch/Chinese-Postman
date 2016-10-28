b4fa40ebd02edad0509243501b2579408bc58fdc
Mitch LeBlanc <supermitch@gmail.com>
2016-10-27 21:28:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
 from setuptools import setup
 
-setup(name='chinesepostman',
-      version='0.1',
-      url='https://github.com/supermitch/Chinese-Postman',
-      author='Mitch Leblanc',
-      packages=['chinesepostman'])
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='chinesepostman',
+    version='0.0.1',
+    description='Chinese-Postman network solver',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    url='https://github.com/supermitch/Chinese-Postman',
+    author='Mitch LeBlanc',
+    author_email='supermitch@gmail.com',
+    packages=['chinesepostman'],
+    license='MIT',
+)
