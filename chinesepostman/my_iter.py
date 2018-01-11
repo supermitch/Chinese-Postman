da70fa572d46d26087f42889fe9de0993628e82d
Mitch LeBlanc <supermitch@gmail.com>
2018-01-11 14:07:32 -0800

    Reformat docstrings

--- chinesepostman/my_iter.py
+++ chinesepostman/my_iter.py
@@ -1,14 +1,9 @@
-"""
-Iterator related helper functions.
-
-"""
+""" Iterator related helper functions. """
 
 def flatten_tuples(iterable):
     """
     Flatten an iterable containing several tuples, into a single tuple.
-
     Sum all tuples, which "extends" them, with empty tuple as start value.
-
     """
     return sum(iterable, ())
 
@@ -16,4 +11,3 @@ def all_unique(iterable):
     """ Returns True if all items in an iterable are unique. """
     seen = set()
     return not any(x in seen or seen.add(x) for x in iterable)
-
