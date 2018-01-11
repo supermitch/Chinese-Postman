d2973e59a7257f45615eadbd23eafd9a98e8a17e
Mitch LeBlanc <supermitch@gmail.com>
2018-01-11 14:08:14 -0800

    Reformat docstring

--- chinesepostman/my_math.py
+++ chinesepostman/my_math.py
@@ -1,9 +1,5 @@
-"""
-Some simple math helpers.
-
-"""
+""" Some simple math helpers. """
 
 def is_even(number):
     """ Return True if a number is even, False if odd. """
     return not number % 2
-
